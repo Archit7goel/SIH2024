33c1c03e4d1217710fbe7ba97cf2e0de616fca52
Grapehool <72132879+Grapehool@users.noreply.github.com>
2024-09-21 12:15:49 +0530

    Revert "Updated all tabs, their layout and predict file"

--- app/(tabs)/prediction.jsx
+++ app/(tabs)/prediction.jsx
@@ -1,43 +1,15 @@
-import { View, Text, Button, Image, StyleSheet, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity,} from 'react-native';
 import { Link } from 'expo-router';
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
-import { images } from 'C:/Users/archi/OneDrive/Desktop/SIH2024/constants/images.js';
-import { Ionicons } from '@expo/vector-icons'; // For icons like menu and profile
-import { createStackNavigator } from '@react-navigation/stack';
 
-
-const Stack = createStackNavigator();
 
 
 const Prediction = ({ navigation }) => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar style="dark" />
-
-      {/* Header */}
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, backgroundColor: '#fff' }}>
-        
-        {/* Menu Icon */}
-        <TouchableOpacity onPress={() => Alert.alert("Menu clicked")}>
-          <Ionicons name="menu" size={40} color="black" />
-        </TouchableOpacity>
-
-        {/* Logo in the Center */}
-        <Image source={require('C:/Users/archi/OneDrive/Desktop/SIH2024/assets/images/logocityforge.png')} style={{ width: 300, height: 80 }} resizeMode="contain" />
-
-
-        {/* Profile Icon */}
-        <TouchableOpacity onPress={() => Alert.alert("Profile clicked")}>
-          <Ionicons name="person-circle-outline" size={40} color="black" />
-        </TouchableOpacity>
-      </View>
-  
-    {/* <ScrollView style={styles.container}> */}
+    <View style={newStyle.container}>
       {/* Delay Prediction */}
       <Link href="/delayprediction" asChild>
-        <TouchableOpacity className="bg-blue-500 p-5 rounded-full items-center my-2">
+        <TouchableOpacity style={newStyle.delay}>
         <View>
           <Text  className='text-blue-50 text-xl'>Delay Prediction</Text>
         </View> 
@@ -46,7 +18,7 @@ const Prediction = ({ navigation }) => {
 
       {/* Injury Prediction */}
       <Link href="/injuryprediction" asChild>
-        <TouchableOpacity className="bg-blue-500 p-5 rounded-full items-center my-2">
+        <TouchableOpacity style={newStyle.injury}>
         <View>
           <Text className='text-blue-50 text-xl'>Injury Prediction</Text>
         </View> 
@@ -56,18 +28,40 @@ const Prediction = ({ navigation }) => {
 
       {/*Cost Overrun Prediction*/}
       <Link href="/overrunprediction" asChild>
-        <TouchableOpacity className="bg-blue-500 p-5 rounded-full items-center my-2">
+        <TouchableOpacity style={newStyle.cost}>
         <View>
           <Text className='text-blue-50 text-xl'>Cost Overrun Prediction</Text>
         </View> 
         </TouchableOpacity>
       </Link>
 
-    </SafeAreaView>
-    // </ScrollView>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const newStyle = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection: 'column',
+  },
+  delay:{
+    backgroundColor: '#f8b195',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex:1
+  },
+  injury:{
+    backgroundColor: '#f67280',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex:1
+  },
+  cost:{
+    backgroundColor: '#c06c84',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex:1
+  }
+});
 
 export default Prediction;
